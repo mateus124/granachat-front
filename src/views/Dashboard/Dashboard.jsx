d0f587ba945c51c94be606195b89589609a6fe00
mateus124 <alvesmateusq00@gmail.com>
2026-03-14 16:37:10 -0300

    feat: dashboard header

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -1,10 +1,22 @@
 import Menu from '../../components/Menu/Menu'
+import styles from './Dashboard.module.css'
+import { IoMdNotifications } from "react-icons/io";
+import { CgProfile } from "react-icons/cg";
 
 const Dashboard = () => {
     return (
-        <>
+        <main className={styles.main}>
             <Menu/>
-        </>
+            <div className={styles.dashboard}>
+                <div className={styles.header}>
+                    <h2>Resumo Financeiro</h2>
+                    <div className={styles.icons}>
+                        <IoMdNotifications size={26} />
+                        <CgProfile size={26} />
+                    </div>
+                </div>
+            </div>
+        </main>
     )
 }
 
